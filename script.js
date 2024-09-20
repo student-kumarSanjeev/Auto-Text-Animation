0a9470951b058296bd3c90da36745fe66c1f5c71
Sanjeev Kumar <sanjeevjanichak@gmail.com>
2024-09-20 20:18:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const containerEl = document.querySelector(".container")
+const careers = ["YouTuber", "Web Developer","Designer","Blogger","Instructor"]
+let careerIndex = 0;
+let characterIndex = 0;
+updateText();
+function updateText(){
+    characterIndex++;
+    containerEl.innerHTML = ` 
+     <h1>I am a ${careers[careerIndex].slice(0,characterIndex)}</h1> 
+     `; 
+   
+     if(characterIndex === careers[careerIndex].length){
+        careerIndex++;
+        characterIndex = 0; 
+     }
+     if(careerIndex === careers.length)
+     {
+        careerIndex = 0; 
+     }
+     setTimeout(updateText,200);
+}
